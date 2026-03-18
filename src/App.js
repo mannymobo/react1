37b8c9b3898ccb2286e2cf4efc0331bb28218f8b
Manny <manny@mobo.co.uk>
2026-03-18 14:40:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Welcome to Mobo Media
+        Welcome to Mobo Media Files
         </p>
         <a
           className="App-link"
